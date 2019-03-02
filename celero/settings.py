dd24021401771cc902f7823dc769aabfde12e33c
alison <alison@metanoia>
2019-03-01 21:06:42 -0300

    Extraidas config de banco de dados
    close #26

--- celero/settings.py
+++ celero/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from functools import partial
 
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from decouple import config, Csv
 
@@ -76,13 +78,13 @@ WSGI_APPLICATION = 'celero.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
+DATABASES = {
+    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
