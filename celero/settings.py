b196c499f904ddb054fdb22933576fe3d1c92b16
alison <alison@metanoia>
2019-03-01 23:07:21 -0300

    Instalar Django Rest framework
    close #32

--- celero/settings.py
+++ celero/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'celero.api'
+    'celero.api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
