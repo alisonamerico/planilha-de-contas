01466fc082ab6b40a82057a5a446d1c4270f7311
alison <alison@metanoia>
2019-03-06 22:52:02 -0300

    Criado campo controle de entradas e saídas
    close #62

--- celero/api/serializers.py
+++ celero/api/serializers.py
@@ -2,10 +2,17 @@ from rest_framework import serializers
 from celero.api import models
 
 
+"""
+ContabilidadeSerializer - Irá fazer a tradução dos objetos implementados no Model Contabilidade
+para visualização dos mesmos no form do DRF.
+"""
+
+
 class ContabilidadeSerializer(serializers.ModelSerializer):
     class Meta:
         fields = (
             'id',
+            'controle',
             'nome',
             'valor',
         )
--- celero/api/views.py
+++ celero/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from celero.api import models
+
 from . import serializers
 
 
@@ -12,7 +13,6 @@ class ContabilidadeViewSet(viewsets.ModelViewSet):
         - Realiza consultas dos objetos no banco de dados;
         - Implementa os campos de pesquisa e filtros de ordenação por nome e valor;
         - Autenticação e permissão para acesso à API.
-
     """
     queryset = models.Contabilidade.objects.all()
     serializer_class = serializers.ContabilidadeSerializer
